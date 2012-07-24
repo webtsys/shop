99cb25a55bf8a5bfd55ef3aca67609ff297740f6
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-07-24 02:01:14 +0200

    Little fix in a regresssion

--- libraries/type_index/categories.php
+++ libraries/type_index/categories.php
@@ -7,7 +7,7 @@ $query=$model['cat_product']->select('where subcat=0');
 while($arr_cat=webtsys_fetch_array($query))
 {
 
-	echo load_view(array($model['cat_product']->components['title']->show_formatted($arr_cat['title']), $model['cat_product']->components['description']->show_formatted($arr_cat['description']).'<p><a href="'.make_fancy_url($base_url, 'shop', 'viewcategory', 'viewcategory', array('IdCat_product' => $arr_cat['IdCat_product']) ).'" class="see">'.$lang['shop']['see_products'].'</a></p>'), 'content');
+	echo load_view(array($model['cat_product']->components['title']->show_formatted($arr_cat['title']), $model['cat_product']->components['description']->show_formatted($arr_cat['description']).'<p><a href="'.make_fancy_url($base_url, 'shop', 'viewcategory', 'viewcategory', array('IdCat_product' => $arr_cat['IdCat_product']) ).'" class="see_products">'.$lang['shop']['see_products'].'</a></p>'), 'content');
 
 }
 
