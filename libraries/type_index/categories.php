92b2bcd37f5e78075556a796745a93e4a8862f34
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-12-02 05:09:07 +0100

    Fix bug #3

--- libraries/type_index/categories.php
+++ libraries/type_index/categories.php
@@ -1,22 +1,15 @@
 <?php
 
-//echo load_view(array($config_shop['title_shop'], $model['config_shop']->components['description_shop']->show_formatted($config_shop['description_shop']) ), 'content');
-
 $query=$model['cat_product']->select('where subcat=0');
 
 while($arr_cat=webtsys_fetch_array($query))
 {
 
-	$title_cat=$model['cat_product']->components['title']->show_formatted($arr_cat['title']);
-	$description_cat=$model['cat_product']->components['description']->show_formatted($arr_cat['description']);
-	$idcat=$arr_cat['IdCat_product'];
-
-	//echo load_view(array($model['cat_product']->components['title']->show_formatted($arr_cat['title']), $model['cat_product']->components['description']->show_formatted($arr_cat['description']).'<p><a href="'.make_fancy_url($base_url, 'shop', 'viewcategory', 'viewcategory', array('IdCat_product' => $arr_cat['IdCat_product']) ).'" class="see_products">'.$lang['shop']['see_products'].'</a></p>'), 'content');
-
-	
-	//ViewCategoriesView($title_cat, $description_cat, $idcat)
-	echo load_view(array($title_cat, $description_cat, $idcat), 'shop/viewcategories');
+	$arr_title_cat[$arr_cat['IdCat_product']]=$model['cat_product']->components['title']->show_formatted($arr_cat['title']);
+	$arr_description_cat[$arr_cat['IdCat_product']]=$model['cat_product']->components['description']->show_formatted($arr_cat['description']);
 	
 }
+
+echo load_view(array($arr_title_cat, $arr_description_cat), 'shop/viewcategories');
 
 ?>
--- views/shop/viewcategories.php
+++ views/shop/viewcategories.php
@@ -1,12 +1,18 @@
 <?php
 
-function ViewCategoriesView($title_cat, $description_cat, $idcat)
+function ViewCategoriesView($arr_title_cat, $arr_description_cat)
 {
 
 	global $base_url, $lang, $model;
+	
+	foreach($arr_title_cat as $idcat => $title_cat)
+	{
+	
+		$description_cat=$arr_description_cat[$idcat];
 
-	echo load_view(array($title_cat, $description_cat.'<p><a href="'.make_fancy_url($base_url, 'shop', 'viewcategory', 'viewcategory', array('IdCat_product' => $idcat) ).'" class="see_products">'.$lang['shop']['see_products'].'</a></p>'), 'content');
+		echo load_view(array($title_cat, $description_cat.'<p><a href="'.make_fancy_url($base_url, 'shop', 'viewcategory', 'viewcategory', array('IdCat_product' => $idcat) ).'" class="see_products">'.$lang['shop']['see_products'].'</a></p>'), 'content');
 
+	}
 }
 
 ?>
