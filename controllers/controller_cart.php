565ca4bbbc22ba3dcf41f11dc28e2921741dcd42
Antonio de la Rosa <webmaster@web-t-sys.com>
2013-02-14 03:07:05 +0100

    Fix in cart

--- controllers/controller_cart.php
+++ controllers/controller_cart.php
@@ -1476,7 +1476,9 @@ function obtain_transport_price($total_weight, $total_price, $idtransport)
 	{
 	
 		$query=webtsys_query('select price from price_transport_price where min_price>='.$total_price.' and idtransport='.$idtransport.' order by price ASC limit 1');
-			
+		
+		echo 'select price from price_transport_price where min_price>='.$total_price.' and idtransport='.$idtransport.' order by price ASC limit 1';
+		
 		list($price_transport)=webtsys_fetch_row($query);
 
 		settype($price_transport, 'double');
