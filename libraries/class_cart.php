0d5d14d8e0d078761a9aad95b8da3948f25382c4
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-09-17 03:08:20 +0200

    Added new methods to class_cart

--- libraries/class_cart.php
+++ libraries/class_cart.php
@@ -14,33 +14,36 @@ class CartClass {
 	public function show_cart()
 	{
 	
+		global $lang;
 		
+		load_lang('shop');
 	
+		echo '<p>'.$lang['shop']['explain_cart_options'].'</p>';
+	
+		//Add plugins for cart that added money to price, for example, taxes or discounts. You can configure taxes or discounts on its plugins admin.
+	
+		
+		
 	}
 	
 	public function modify_attributes_cart()
 	{
 	
-		
+		//here modify attributes for example, num units or the chosen unit with special attributes, for example, color.
 	
 	}
 	
 	public function add_to_cart($idproduct, $arr_details=array(), $price=0, $special_offer=0, $redirect=1)
 	{
 	
-		
-	
-	}
-	
-	public function payment_address()
-	{
-	
-		
+		//Add product to cart, if the product have attributes, config here.
 	
 	}
 	
 	public function execute_plugins_payment()
 	{
+	
+		//Here, plugins that you need configure for add to the price, for example, transport.
 	
 		
 	
@@ -49,7 +52,7 @@ class CartClass {
 	public function payment_gateway()
 	{
 	
-		
+		//Here define the payment and notify that the product was paid.
 	
 	}
 
