949e44c9589276ca47d2abdb6e99264defae89d8
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-09-23 14:33:18 +0200

    Added vat plugin

---
+++ plugins/vat/cart/vat.php
@@ -0,0 +1,30 @@
+<?php
+
+class VatCartClass {
+
+	public function __construct()
+	{
+	
+	}
+	
+	//Here prepare the config of the plugin.
+	
+	public function prepare_plugin()
+	{
+	
+	}
+	
+	public function show()
+	{
+	
+	}
+	
+	public function return_value_modified()
+	{
+	
+	}
+
+}
+
+
+?>
