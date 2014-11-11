4b5bf26f6d2f32f4979bb86dfd5fd73d44c407c8
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-11-11 14:04:33 +0100

    Fixes on ordershop

--- views/shop/ordershop.php
+++ views/shop/ordershop.php
@@ -3,7 +3,7 @@
 function OrderShopView()
 {
 
-
+global $config_shop, $model;
 
 ?>
 <!DOCTYPE html>
@@ -21,10 +21,10 @@ function OrderShopView()
 		<table style="width:100%;">
 			<tr>
 				<td style="width:50%;">
-					Logo
+					<img src="<?php echo $model['config_shop']->components['image_bill']->show_image_url($config_shop['image_bill']); ?>" border="0"/>
 				</td>
 				<td style="width:50%;">
-					Address shop
+					<?php echo $config_shop['bill_data_shop']; ?>
 				</td>
 			</tr>
 		</table>
