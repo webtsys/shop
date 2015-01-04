d03373bfb05b9aac0fc8ddc007ca46d8fa582ad1
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-01-04 02:33:22 +0100

    Added viewcategory

---
+++ controllers/controller_viewcategory.php
@@ -0,0 +1,122 @@
+<?php
+
+class  ViewcategorySwitchClass extends ControllerSwitchClass
+{
+
+	function index($idcat_product=0, $title_slugify='')
+	{
+
+		//global $user_data, PhangoVar::$model, $ip, PhangoVar::$lang, $config_data, PhangoVar::$base_path, PhangoVar::$base_url, $cookie_path, $arr_block, $prefix_key, $block_title, $block_content, $block_urls, $block_type, $block_id, $config_data, ConfigShop::$config_shop, PhangoVar::$lang_taxes;
+
+		load_lang('shop');
+		load_model('shop');
+		load_libraries(array('config_shop'), PhangoVar::$base_path.'modules/shop/libraries/');
+		load_libraries(array('pages', 'forms/selectmodelformbyorder', 'generate_admin_ng', 'utilities/hierarchy_links'));
+
+		/*$cont_index='';
+
+		$arr_block=select_view(array('shop'));*/
+
+		//Load page...
+
+		settype($idcat_product, 'integer');
+		
+		$num_news=ConfigShop::$config_shop['num_news'];
+		
+		//Obtain category...
+		
+		$arr_cat=PhangoVar::$model['cat_product']->select_a_row($idcat_product, array(), 1);
+		
+		settype($arr_cat['IdCat_product'], 'integer');
+		
+		//$where_sql='where idcat='.$arr_cat['IdCat_product'];
+		$where_sql='where product_relationship.idcat_product='.$arr_cat['IdCat_product'];
+		
+		if($arr_cat['IdCat_product']==0)
+		{
+		
+			$arr_cat['title']=PhangoVar::$lang['shop']['all_products'];
+			$arr_cat['description']=PhangoVar::$lang['shop']['desc_all_products'];
+			$arr_cat['subcat']=0;
+			$arr_cat['view_only_mode']=ConfigShop::$config_shop['view_only_mode'];
+			$where_sql='';
+		
+		}
+		else
+		{
+		
+			$arr_cat['title']=I18nField::show_formatted($arr_cat['title']);
+			$arr_cat['description']=I18nField::show_formatted($arr_cat['description']);
+		
+		}
+		
+		PhangoVar::$model['product']->related_models=array('product_relationship' => array('idproduct'));
+		
+		PhangoVar::$model['product']->create_form();
+		
+		$url_options=make_fancy_url(PhangoVar::$base_url, 'shop', 'viewcategory', array($idcat_product, $arr_cat['title']));
+		
+		$arr_fields_orders=array('date', 'title_'.$_SESSION['language']);
+		$arr_fields_search=array('title_'.$_SESSION['language']);
+		
+		PhangoVar::$model['product']->forms['title_'.$_SESSION['language']]->label=PhangoVar::$lang['common']['title'];
+		PhangoVar::$model['product']->forms['date']->label=PhangoVar::$lang['common']['date'];
+		
+		$cont_search='';
+		
+		if(!isset($_GET['order_desc']))
+		{
+		
+			$_GET['order_desc']=1;
+		
+		}
+		
+		ob_start();
+		
+		list($where_sql, $arr_where_sql, $location, $arr_order)=SearchInField('product', $arr_fields_orders, $arr_fields_search, $where_sql, $url_options, 0);
+		
+		$cont_search=ob_get_contents();
+		
+		ob_end_clean();
+		
+		$where_sql.=$arr_where_sql.' order by '.$location.'`'.$_GET['order_field'].'` '.$arr_order[$_GET['order_desc']];
+		
+		$total_elements=PhangoVar::$model['product']->select_count($where_sql, 'IdProduct');
+		
+		//Now, set where with searchs...
+		
+		//Now select products...
+		
+		$arr_product=PhangoVar::$model['product']->select_to_array($where_sql.' limit '.$_GET['begin_page'].', '.$num_news, array());
+		
+		//Select ids...
+		
+		$arr_id=array_keys($arr_product);
+		
+		$arr_id[]=0;
+		
+		//Select images...
+		
+		$arr_photo=array();
+		
+		$query=PhangoVar::$model['image_product']->select('where idproduct IN (\''.implode("', '", $arr_id).'\') and principal=1', array('photo', 'idproduct'), true);
+
+		while(list($photo, $idproduct)=webtsys_fetch_row($query))
+		{
+
+			$arr_photo[$idproduct]=$photo;
+
+		}
+		
+		echo load_view(array($idcat_product, $arr_cat, $arr_product, $arr_photo, $cont_search, $total_elements), 'shop/viewcategory');
+		
+		$cont_index.=ob_get_contents();
+
+		ob_end_clean();
+
+		//$arr_block($title_category, $cont_index, $block_title, $block_content, $block_urls, $block_type, $block_id, $config_data, '');
+		echo load_view(array($arr_cat['title'], $cont_index), 'home');
+	}
+}
+
+?>
--- views/shop/viewcategory.php
+++ views/shop/viewcategory.php
@@ -1,24 +1,40 @@
 <?php
 
-function ViewCategoryView($arr_cat, $arr_product, $arr_photo, $search_product, $total_elements)
+function ViewCategoryView($idcat_product, $arr_cat, $arr_product, $arr_photo, $search_product, $total_elements)
 {
 
-global $lang, $config_shop, $base_url, $model;
+//global PhangoVar::$lang, ConfigShop::$config_shop, PhangoVar::$base_url, PhangoVar::$model;
 
-$title_category=$arr_cat['title'];
-$num_news=$config_shop['num_news'];
+$arr_hierarchy_links=hierarchy_links('cat_product', 'subcat', 'title', $idcat_product);
+
+echo load_view(array($arr_hierarchy_links, 'shop', 'viewcategory', 'IdCat_product', array(), 0), 'common/utilities/hierarchy_links');
 
 ob_start();
 
 ?>
-<form method="get" action="<?php echo make_fancy_url($base_url, 'shop', 'viewcategory', $title_category, array()); ?>">
+
+
+
+<?php
+
+PhangoVar::$arr_cache_header[]=ob_get_contents();
+
+ob_end_clean();
+
+$title_category=$arr_cat['title'];
+$num_news=ConfigShop::$config_shop['num_news'];
+
+ob_start();
+
+?>
+<form method="get" action="<?php echo make_fancy_url(PhangoVar::$base_url, 'shop', 'viewcategory', array(0, slugify($title_category))); ?>">
 	<p>
 	<?php
 
-	echo $lang['shop']['select_category_shop'].': '.SelectModelFormByOrder('IdCat_product', '', $_GET['IdCat_product'], 'cat_product', 'title', 'subcat', $where='');
+	echo PhangoVar::$lang['shop']['select_category_shop'].': '.SelectModelFormByOrder('IdCat_product', '', $idcat_product, 'cat_product', 'title', 'subcat', $where='');
 
 	?>
-	<input type="submit" value="<?php echo $lang['shop']['choose_category']; ?>"/>
+	<input type="submit" value="<?php echo PhangoVar::$lang['shop']['choose_category']; ?>"/>
 	</p>
 <form>
 <?php
@@ -49,18 +65,18 @@ foreach($arr_product as $key_prod => $product)
 
 		$offer=$product['special_offer'];
 	
-		$add_tax_offer=calculate_taxes($idtax, $product['special_offer']);
-		$offer+=$add_tax_offer;
+		/*$add_tax_offer=calculate_taxes($idtax, $product['special_offer']);
+		$offer+=$add_tax_offer;*/
 
-		$price= '<strong>'.$lang['shop']['offer'].'</strong> <span style="text-decoration: line-through;">'.MoneyField::currency_format($price_real).' </span> -> '.MoneyField::currency_format($offer);
+		$price= '<strong>'.PhangoVar::$lang['shop']['offer'].'</strong> <span style="text-decoration: line-through;">'.MoneyField::currency_format($price_real).' </span> -> '.MoneyField::currency_format($offer);
 
 	}
 	
 	$stock=$product['stock'];
 	$stock_txt='';
 	
-	$arr_stock[0]=$lang['shop']['no_stock'];
-	$arr_stock[1]=$lang['shop']['in_stock'];
+	$arr_stock[0]=PhangoVar::$lang['shop']['no_stock'];
+	$arr_stock[1]=PhangoVar::$lang['shop']['in_stock'];
 
 	if($product['about_order']==0)
 	{
@@ -71,7 +87,7 @@ foreach($arr_product as $key_prod => $product)
 	else
 	{
 
-		$stock_txt=$lang['shop']['served_on_request'];
+		$stock_txt=PhangoVar::$lang['shop']['served_on_request'];
 
 	}
 	
@@ -86,7 +102,7 @@ foreach($arr_product as $key_prod => $product)
 				if(isset($arr_photo[$key_prod]))
 				{
 				?>
-				<img src="<?php echo $model['image_product']->components['photo']->show_image_url('mini_'.$arr_photo[$key_prod]); ?>" />
+				<img src="<?php echo PhangoVar::$model['image_product']->components['photo']->show_image_url('mini_'.$arr_photo[$key_prod]); ?>" />
 				<?php
 				}
 				?>
@@ -94,27 +110,27 @@ foreach($arr_product as $key_prod => $product)
 			<div class="description_product">
 				<?php echo I18nField::show_formatted($product['description']); ?>
 				<br /><br />
-				<strong><?php echo $lang['shop']['pvp']; ?>:</strong> <?php echo $price; ?>
+				<strong><?php echo PhangoVar::$lang['shop']['pvp']; ?>:</strong> <?php echo $price; ?>
 				<br />
 				<?php echo $stock_txt; ?>
+				<!--<br />
+				<strong><?php echo PhangoVar::$lang['shop']['weight_in_kg']; ?>: </strong> <?php echo $product['weight']; ?> <?php echo PhangoVar::$lang['shop']['kg']; ?>-->.
 				<br />
-				<strong><?php echo $lang['shop']['weight_in_kg']; ?>: </strong> <?php echo $product['weight']; ?> <?php echo $lang['shop']['kg']; ?>.
 				<br />
-				<br />
-				<a href="<?php echo make_fancy_url($base_url, 'shop', 'viewproduct', $title_product, array('IdProduct' => $idproduct) ); ?>" class="see">
-					<?php echo $lang['shop']['see_product']; ?>
+				<a href="<?php echo make_fancy_url(PhangoVar::$base_url, 'shop', 'viewproduct', array($idproduct, $title_product) ); ?>" class="see">
+					<?php echo PhangoVar::$lang['shop']['see_product']; ?>
 				</a>
 				<?php
-			if($config_shop['view_only_mode']==0 && $arr_cat['view_only_mode']==0 && $stock==1)
+			/*if(ConfigShop::$config_shop['view_only_mode']==0 && $arr_cat['view_only_mode']==0 && $stock==1)
 			{
 			?>
-			<a onclick="javascript:buy_product(<?php echo $idproduct; ?>); return false;" href="<?php echo make_fancy_url($base_url, 'shop', 'buy', 'buy_product', array('IdProduct' => $idproduct) ); ?>" class="ship"><span id="text_buy_<?php echo $idproduct; ?>"><?php echo $lang['shop']['buy_product']; ?></span>
+			<a onclick="javascript:buy_product(<?php echo $idproduct; ?>); return false;" href="<?php echo make_fancy_url(PhangoVar::$base_url, 'shop', 'buy', 'buy_product', array('IdProduct' => $idproduct) ); ?>" class="ship"><span id="text_buy_<?php echo $idproduct; ?>"><?php echo PhangoVar::$lang['shop']['buy_product']; ?></span>
 			</a>
-			<img id="loading_buy_<?php echo $idproduct; ?>" src="<?php echo $base_url; ?>/media/default/images/loading.gif" alt="<?php echo $lang['shop']['buying_product']; ?>" style="display: none;" />
+			<img id="loading_buy_<?php echo $idproduct; ?>" src="<?php echo PhangoVar::$base_url; ?>/media/default/images/loading.gif" alt="<?php echo PhangoVar::$lang['shop']['buying_product']; ?>" style="display: none;" />
 			
-			<br clear="all" /><div id="show_process_buying"><p id="buying_<?php echo $idproduct; ?>" style="display: none;"><span class="error"><?php echo $lang['shop']['buying_product']; ?></span></p><p id="sucess_buy_<?php echo $idproduct; ?>" style="display: none;"><span class="error"><?php echo $lang['shop']['success_buy']; ?></span></p></div>
+			<br clear="all" /><div id="show_process_buying"><p id="buying_<?php echo $idproduct; ?>" style="display: none;"><span class="error"><?php echo PhangoVar::$lang['shop']['buying_product']; ?></span></p><p id="sucess_buy_<?php echo $idproduct; ?>" style="display: none;"><span class="error"><?php echo PhangoVar::$lang['shop']['success_buy']; ?></span></p></div>
 			<?php
-			}
+			}*/
 			?>
 			</div>
 		</div>
@@ -129,16 +145,16 @@ foreach($arr_product as $key_prod => $product)
 	if($z==0)
 	{
 
-		echo '<p>'.$lang['shop']['no_products_in_category'].'</p>';
+		echo '<p>'.PhangoVar::$lang['shop']['no_products_in_category'].'</p>';
 
 	}
 	else
 	{
 
 		
-		$url_next=make_fancy_url($base_url, 'shop', 'viewcategory', $title_category, array('IdCat_product' => $_GET['IdCat_product']) );
+		$url_next=make_fancy_url(PhangoVar::$base_url, 'shop', 'viewcategory', array($idcat_product, slugify($title_category)) );
 		
-		echo '<p>'.$lang['common']['pages'].': '.pages( $_GET['begin_page'], $total_elements, $num_news, $url_next).'</p>';
+		echo '<p>'.PhangoVar::$lang['common']['pages'].': '.pages( $_GET['begin_page'], $total_elements, $num_news, $url_next).'</p>';
 
 	}
 
